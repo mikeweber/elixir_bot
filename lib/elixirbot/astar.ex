add2c9622241a6b998fbe16707cbaf62bca6b618
Mike Weber <mike@weberapps.com>
2018-01-17 22:16:21 -0600

    Trying to get A* tests passing

--- lib/elixirbot/astar.ex
+++ lib/elixirbot/astar.ex
@@ -9,46 +9,55 @@ defmodule Astar do
   end
 
   def inspector(graph) do
-    Logger.info(inspect graph)
+    IO.inspect(graph)
     graph
   end
 
+  # What to do when one traversal is complete, but there are others to check?!
   def traverse_graph({[], came_from}, _, _), do: {[], came_from}
   def traverse_graph({frontier, came_from}, graph, target) do
     {current_node_name, frontier} = PriorityQueue.get(frontier)
+    current_node = Graph.get_node(graph, current_node_name)
 
-    deep_traverse({frontier, came_from}, graph, target, Graph.get_node(graph, current_node_name))
+    deep_traverse({frontier, came_from}, graph, target, current_node)
   end
 
   def deep_traverse(state, _, target, target), do: state
-  def deep_traverse(state, _,      _, nil), do: state
-  def deep_traverse({frontier, came_from} = state, graph, %{ entity: target_entity } = target, %GraphNode{ adjacents: adjacents, name: current_name } = current) do
+  def deep_traverse({[], came_frome} = state, _, _, _), do: state
+  def deep_traverse(state, graph, %{ entity: target_entity } = target, %GraphNode{ adjacents: adjacents, name: current_name } = current) do
     adjacents
-    |> Enum.reduce(state, fn({next_name, {next_weight, next_entity}}, state)->
+    |> Enum.reduce(state, fn({next_name, {next_weight, next_entity}}, {frontier, came_from})->
       new_cost = (came_from[current_name] |> elem(0)) + next_weight
       prev_node = came_from[next_name]
 
       if prev_node && elem(prev_node, 0) <= new_cost do
         state
       else
+        IO.inspect("Adding #{next_name} to frontier")
         {
           frontier  |> PriorityQueue.add({new_cost + heuristic(target_entity, next_entity), next_name}),
           came_from |> Map.put(next_name, {new_cost, current})
         }
       end
     end)
+    |> inspector
     |> traverse_graph(graph, target)
   end
 
   def heuristic(%{ x: x1, y: y1 }, %{ x: x2, y: y2 }) do
-    :math.pow(x1 - x2, 2) + :math.pow(y1 - y2, 2)
+    (:math.pow(x1 - x2, 2) + :math.pow(y1 - y2, 2)) |> :math.sqrt
   end
   def heuristic(_, _), do: 0
 
   def rebuild_path(        _,      _,    nil), do: []
   def rebuild_path(        _, %{ name: origin }, %{ name: origin }), do: []
   def rebuild_path(came_from, origin, current) do
-    rebuild_path(came_from, origin, (came_from[current.name]) |> elem(1)) ++ [current]
+    IO.inspect("came from current: #{inspect came_from[current.name]}")
+    if came_from[current.name] do
+      rebuild_path(came_from, origin, (came_from[current.name]) |> elem(1)) ++ [current]
+    else
+      [current]
+    end
   end
 end
 
--- test/elixirbot/astar_test.exs
+++ test/elixirbot/astar_test.exs
@@ -28,18 +28,28 @@ defmodule AstarTest do
 
   test "can navigate a graph of nodes" do
     graph  = %Graph{}
-    node_a = %GraphNode{ name: :a, entity: %{ x:  0.0, y:  0.0 } }
-    node_b = %GraphNode{ name: :b, entity: %{ x:  0.0, y: 10.0 } }
-    node_c = %GraphNode{ name: :c, entity: %{ x: 10.0, y: 10.0 } }
-    node_d = %GraphNode{ name: :d, entity: %{ x: 10.0, y:  3.0 } }
+    a = %{ x:  0.0, y:  0.0 }
+    b = %{ x:  0.0, y: 10.0 }
+    c = %{ x: 10.0, y: 10.0 }
+    d = %{ x:  7.0, y: 13.0 }
+
+    node_a = %GraphNode{ name: :a, entity: a }
+    node_b = %GraphNode{ name: :b, entity: b }
+    node_c = %GraphNode{ name: :c, entity: c }
+    node_d = %GraphNode{ name: :d, entity: d }
 
     {graph, node_a, node_b} = graph |> Graph.add_edge(node_a, node_b, 10.0)
     {graph, node_a, node_c} = graph |> Graph.add_edge(node_a, node_c, 14.4)
     {graph, node_b, node_c} = graph |> Graph.add_edge(node_b, node_c, 10.0)
-    {graph, node_b, node_d} = graph |> Graph.add_edge(node_b, node_d,  3.0)
-    {graph,      _, node_d} = graph |> Graph.add_edge(node_c, node_d,  7.0)
+    {graph, node_b, node_d} = graph |> Graph.add_edge(node_b, node_d,  7.6)
+    {graph, node_c, node_d} = graph |> Graph.add_edge(node_c, node_d,  4.2)
 
-    assert [node_b, node_d] == Astar.find_path(node_a, graph, node_d)
+    assert [:b, :c]     == Map.keys(node_a.adjacents)
+    assert [:a, :c, :d] == Map.keys(node_b.adjacents)
+    assert [:a, :b, :d] == Map.keys(node_c.adjacents)
+    assert [:b, :c]     == Map.keys(node_d.adjacents)
+
+    assert [node_b, node_d] |> Enum.map(&(&1.name)) == Astar.find_path(node_a, graph, node_d) |> Enum.map(&(&1.name))
   end
 
   test "can add new points to a pre-built graph" do
@@ -64,16 +74,38 @@ defmodule AstarTest do
     assert_adjacents node_c, [a ,b, d]
     assert_adjacents node_d, [b, c]
 
-    assert [b, d] == Astar.find_path(node_a, graph, node_d) |> Enum.map(&(&1.entity))
+    assert [node_b, node_d] |> Enum.map(&(&1.name)) == Astar.find_path(node_a, graph, node_d) |> Enum.map(&(&1.name))
 
     graph_with_points = GameMap.append_graph(graph, map, [start_point, end_point])
+
     node_start = Graph.get_node(graph_with_points, Entity.to_atom(start_point))
     node_end   = Graph.get_node(graph_with_points, Entity.to_atom(end_point))
+    node_a     = Graph.get_node(graph_with_points, Entity.to_atom(a))
+    node_b     = Graph.get_node(graph_with_points, Entity.to_atom(b))
+    node_c     = Graph.get_node(graph_with_points, Entity.to_atom(c))
+    node_d     = Graph.get_node(graph_with_points, Entity.to_atom(d))
 
     assert_adjacents node_start, [a, b]
     assert_adjacents node_end, [c, d]
+    assert_adjacents node_a, [b, c, start_point]
+    assert_adjacents node_b, [a, c, d, start_point]
+    assert_adjacents node_c, [a, b, d, end_point]
+    assert_adjacents node_d, [b, c, end_point]
 
-    assert [a, c, end_point] == Astar.find_path(node_start, graph_with_points, node_end) |> Enum.map(&(&1.entity))
+    assert [node_a, node_c, node_end] |> Enum.map(&(&1.name)) == Astar.find_path(node_start, graph_with_points, node_end) |> Enum.map(&(&1.name))
+  end
+
+  test "can add children" do
+    graph = %Graph{}
+    a = %Planet{ id: 0, x: 0.0, y: 0.0, radius: 5.0 }
+    point = %Position{ x: 7.0, y: 0.0 }
+    graph = Graph.add_node(graph, %GraphNode{ name: :planet0, entity: a })
+    a_node = Graph.get_node(graph, :planet0)
+    point_node = %GraphNode{ name: Entity.to_atom(point), entity: point }
+
+    assert %{} == a_node.children
+    a_node = GraphNode.add_child(a_node, point_node)
+    assert a_node.children == %{ point_node.name => point_node }
   end
 
   def assert_adjacents(%{ adjacents: adjacents }, expected_entities) do
